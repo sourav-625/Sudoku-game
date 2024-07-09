f4b2265fd9dbb5c67ba1571a72fd7d79d6f8b3e6
Sourav Pati <k0259.mpsbls@gmail.com>
2024-07-09 20:10:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
             cell.style.border = '1px solid red';
             if (sudoku[i][j] != 0) {
                 cell.textContent = sudoku[i][j];
-                cell.setAttribute('data-fixed', true);
+                cell.readOnly = true;
             }
             row.appendChild(cell);
         }
